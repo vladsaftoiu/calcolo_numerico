fbe268587093738670a8b918495e7e0942bd2fb0
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-03-21 09:35:45 +0100

    docs and some guards

--- finite_difference.m
+++ finite_difference.m
@@ -1,16 +1,32 @@
 function fd = finite_difference( x, f )
-% Calculates the finite difference of a function f over an input
-% vector x; a check to verify the abscissas are different is performed
+% FINITE_DIFFERENCE 
+% 	Calculates the finite difference of a function F over an input
+% 	vector X; in order to correctly work:
+% 		* X and F must have same size;
+% 		* elements of X must be different;
 
-% guard: vectors have same size
-% guard: abscissas are different
-% guard: vector x is ordered
+    x = sort(x);
 
-fd = f;
-n = length(x);
+    if (length(x) ~= length(f))
+        error('input vectors must be same size')
+    end
+    if (~are_distinct(x))
+        error('input abscissas must be distinct')
+    end 
 
-for j = 1 : n
-	for i = n : -1 : j+1
-		fd(i) = (fd(i) - fd(i-1)) / (x(i) - (x(i-j)));
-	end
+    fd = f;
+    n = length(x);
+
+    for j = 1 : n
+        for i = n : -1 : j+1
+            fd(i) = (fd(i) - fd(i-1)) / (x(i) - (x(i-j)));
+        end
+    end
+end
+
+function y = are_distinct( xs )
+    y = true;
+    for i = 1 : 1 : length(xs)-1
+        y = xs(i)-xs(i+1) ~= 0;
+    end
 end
