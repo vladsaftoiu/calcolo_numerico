1b005e8db03afaaf98c4a9713881ac143ab81e9a
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-03-21 08:51:40 +0100

    renge can now map to array

--- renge_test.m
+++ renge_test.m
@@ -1,9 +1,13 @@
-function en = renge_test( n )
+function en = renge_test( ns )
+	en = arrayfun(@(x) inner_renge_test(x), ns)
+end
+
+function ien = inner_renge_test( n )
 
 	abscissas = generate_abscissas(-5, 5, n);
 
 	fx = arrayfun(@(x) 1/(1+x^2), abscissas);
 	fd = finite_difference(abscissas, fx);
 	px = arrayfun(@(x) horner_general(fd, x, abscissas), abscissas);
-    en = max(abs(px - fx))
+    ien = max(abs(px - fx));
 end
