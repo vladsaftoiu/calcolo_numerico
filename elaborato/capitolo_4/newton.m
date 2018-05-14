a271a7ae43185d5d55158d34b183218c08b99e3b
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-05-14 20:09:14 +0200

    missing braces

--- elaborato/capitolo_4/newton.m
+++ elaborato/capitolo_4/newton.m
@@ -7,15 +7,15 @@ function [y] = newton( xi, fi, x)
     n = length(xi)-1; % grado del polinomio interpolante
 
     %calcolo le differenze divise
-    for i = 1 : n
+    for i = 1 : n+1
         for j = n+1 : -1 : i+1
-            fi(j) = (fi(j)-fi(j-1)) / xi(j) - xi(j-i);
+            fi(j) = (fi(j)-fi(j-1)) / (xi(j) - xi(j-i));
         end
     end
     
     y = fi(n+1) * ones(size(x));
-    for i = n : -1 : 1
-        y = y.*(x-xi(i)) + fi(i);
+    for l = n+1 : -1 : 1
+        y = y.*(x-xi(l)) + fi(l);
     end
 
 end
