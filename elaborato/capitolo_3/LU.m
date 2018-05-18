0c9085037a23e7c53ed11174e9000b2168775d0a
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-05-18 21:01:05 +0200

    lol u

---
+++ elaborato/capitolo_3/LU.m
@@ -0,0 +1,16 @@
+function [A] = LU( A )
+%LU determina, se possibile, la fattorizzazione LU della matrice A in
+%ingresso
+%   A: matrice da fattorizzare
+
+    n = length(A); % dimensione matrice
+   
+    for i = 1 : n-1
+        if A(i,i)==0
+            error('la mtraice non è fattorizzabile LU')
+        end
+        
+        A(i+1:n,i) = A(i+1:n,i)/A(i,i);
+        A(i+1:n,i+1:n) = A(i+1:n,i+1:n) -A(i+1:n,i)*A(i,i+1:n);
+    end
+end
