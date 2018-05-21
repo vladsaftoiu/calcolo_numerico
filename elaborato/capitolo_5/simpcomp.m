bd6ae05f8ea0bb5a8db58ee5763dcae986564df7
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-05-21 06:41:10 +0200

    cap 5

--- elaborato/capitolo_5/simpcomp.m
+++ elaborato/capitolo_5/simpcomp.m
@@ -1,14 +1,15 @@
-function [If] = trapcomp( n, a, b, fun )
-%TRAPCOMP( n, a, b, fun ) 
+function [If] = simpcomp( n, a, b, fun )
+%SIMPCOMP( n, a, b, fun ) 
 % calcola l'integrale tra a e b della function fun passata come parametro
-% usando il metodo dei trapezi
+% usando il metodo di simpson
 %   n: numero di intervalli equidistanti della partizione
 %   a: estremo destro intervallo 
 %   b: estremo sinistro intervallo
 %   fun: funzione di cui approssimare l'integrale
 
-    x = linspace(a,b,n+1); %vettore delle ascisse equidistanti
+    m = 2*n;
+    x = linspace(a,b,m+1); %vettore delle ascisse equidistanti
     f = feval(fun, x);
     
-    If = (b-a)/n * (sum(f) -0.5*(f(1)+f(end)));
+    If = (b-a)/(3*m) * (4*sum(f(2:2:m+1)) +2*sum(f(1:2:m+1)) - f(1) - f(end));
 end
