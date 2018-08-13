8197e64a05c70ac0ac81ac9e1e486cd7fb73274d
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-08-13 16:36:02 +0200

    bisection and es. 2.1

---
+++ elaborato/capitolo_2/bisection.m
@@ -0,0 +1,25 @@
+function [x] = newton( a, b, f, tolx )
+
+    fa = feval( f, a );
+    fb = feval( f, b );
+    x = (a+b) / 2;
+    fx = feval( f, x );
+    imax = ceil( log2(b-a) - log2(tolx) );
+    
+    for i=2:imax
+        f1x = abs( (fb-fa)/(b-a) );
+        if ( abs(fx) <= tolx*f1x )
+            break
+        elseif fa*fx<0
+            b = x;
+            fb = fx;
+        else
+            a = x;
+            fa = fx;
+        end
+        
+        x = (a+b)/2;
+        fx = feval(f,x);
+    end
+end
+
