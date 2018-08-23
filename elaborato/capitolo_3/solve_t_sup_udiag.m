e3add664e9fc9617db277a90af2efd66458b698e
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-08-23 19:20:40 +0200

    solve tsup udiag

---
+++ elaborato/capitolo_3/solve_t_sup_udiag.m
@@ -0,0 +1,16 @@
+function [ x ]  = solve_t_sup_udiag( A, b )
+%SOLVE_T_SUP_UDIAG returns x that solves the sistem Ax=b given that:
+%  A is a square matrix upper triangular with its diagonal set to 1
+%  b is the vector of initial equality values
+
+    if (~istriu(A)), error('A is not upper triangular'), end
+    
+    x = b;
+    n = size(A);
+    
+    for i=n : -1 : 1 
+       for j=i+1 : n
+           x(i) = x(i) - A(i,j) * x(j);
+       end
+    end
+end
