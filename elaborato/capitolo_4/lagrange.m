d12bf273f8dac7f2dea179a292b6b32d167211ca
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-08-30 19:23:25 +0200

    cosmetics

--- elaborato/capitolo_4/lagrange.m
+++ elaborato/capitolo_4/lagrange.m
@@ -1,28 +1,29 @@
-function [y] = lagrange( xi, fi, x )
+function [ y ] = lagrange( xi, fi, x )
 %LAGRANGE calcola il polinomio interpolante in forma di Lagrange
 %   xi: vettore delle ascisse
 %   fi: vettore delle ordinate
-%   x: punto da calcolare  
+%   x: vettore dei punti da calcolare  
     
-    y = zeros(1, length(x));
+    m = length(x);
+    y = zeros(1, m);
     
-    for k = 1 : length(x)
-        l = lagrange_c( xi,  x(k));
+    for k = 1 : m
+        l = lagrange_c( xi,  x(k) );
         y(k) = sum(fi.*l);
     end
 end
 
-function [y] = lagrange_c( xi, x )
+function [ y ] = lagrange_c( xi, x )
 %LAGRANGE_C calcola i coefficienti Lkn di lagrange
 %   xi: vettore delle ascisse
-%   x: punto da calcolare
+%   x: vettore dei punti da calcolare
 
     n = length(xi);
     y = ones(1, n);
     for k = 1 : n
         for j = 1 : n
             if (k~=j) 
-               y(k) = y(k) * (x-xi(j))/( xi(k)-xi(j) );
+               y(k) = y(k) * ( x-xi(j ))/( xi(k)-xi(j) );
             end
         end
     end
