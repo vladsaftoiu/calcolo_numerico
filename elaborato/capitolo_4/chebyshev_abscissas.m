0a543801e153503e3be5d526ae5c95fe947a322e
Vlad Saftoiu Alexandru <vlad.saftoiu@dedalus.eu>
2018-08-30 19:31:21 +0200

    exercise 4.6 chebyshev abscissas

---
+++ elaborato/capitolo_4/chebyshev_abscissas.m
@@ -0,0 +1,8 @@
+function [ xi ] = chebyshev_abscissas( n, a, b )
+    
+    xi = zeros(n+1, 1);
+    
+    for i = 0 : n
+        xi(n+1-i) = (a+b) / 2 + cos(pi * (2*i+1) / (2 * (n+1)))*(b-a)/2;
+    end
+end
